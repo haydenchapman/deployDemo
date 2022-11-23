511b7329ccfe16ef4d757aaf5edf67ff88565d25
haydenchapman <hayden@haydenchapman.com>
2022-11-23 15:53:25 -0600

    add btn2 functions

--- public/main.js
+++ public/main.js
@@ -3,3 +3,7 @@ const btn = document.querySelector('button')
 const clickHandler = () => alert('The cat name is: Rascal')
 
 btn.addEventListener('click', clickHandler)
+
+const btn2 = document.querySelector('#button2')
+
+btn2.addEventListener('click', getCat)
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+//yo server stuff
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(express.json())
+app.use(cors())
 
 app.use(express.static(`${__dirname}/public`))
 
 app.listen(4000,
    () => console.log(`Docked at PORT: 4000`)
 )
+
+const getCat = () => {
+    axios.get('http://localhost:4000/api/cat')
+    .then(res => {
+        alert(res.data)
+    })
+    .catch(arr => {
+        console.log(arr)
+    })
+}
+
+app.get('/api/cat', (req, res) => {
+    res.status(200).send('Macy')
+})
+
 
